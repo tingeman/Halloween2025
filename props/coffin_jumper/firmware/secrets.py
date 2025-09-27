fa9d568f6396485dc20fca8828a5c1c75b145bd5
tingeman <thin@dtu.dk>
2025-09-27 10:09:31 +0200

    Added first coffin jumper firmware testing mqtt messaging RX (broker uptime) and TX (telemetry).
    Includes simple plugin, which displays telemetry and availability status.

---
+++ props/coffin_jumper/firmware/secrets.py
@@ -0,0 +1,8 @@
+WIFI_SSID      = "MyNewFancyWiFi"
+WIFI_PASSWORD  = "1123581321"
+MQTT_HOST      = "10.67.1.135"
+MQTT_USER      = "device"
+MQTT_PASSWORD  = "change-me-device"
+MQTT_PORT      = 1883
+
+CLIENT_ID      = "esp32-coffin-jumper-01"
